5c08e6df8cfd5c41d717348581ff4b3e20a740b6
titohajili <maqatoto963@gmail.com>
2024-08-07 20:33:34 +0400

    Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,13 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+/*=============== CHANGE BACKGROUND HEADER ============*/
+
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+
+window.addEventListener('scroll', bgHeader)
